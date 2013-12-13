e3a6343e374537a0f20be266443fed35a8dc4038
bingqiu.yuan <bingqiu.yuan@dianping.com>
2013-12-13 14:52:59 +0800

    Signed-off-by: bingqiu.yuan <bingqiu.yuan@dianping.com>

---
+++ ba-finance-exchange-api/src/main/java/com/dianping/ba/finance/exchange/api/dtos/ShopFundAccountDTO.java
@@ -0,0 +1,45 @@
+package com.dianping.ba.finance.exchange.api.dtos;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bingqiu.yuan
+ * Date: 13-12-13
+ * Time: 下午2:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShopFundAccountDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * CustomerGlobalId
+     */
+    private String customerGlobalId;
+    /**
+     * CompanyGlobalId
+     */
+    private String companyGlobalId;
+    /**
+     * 分店号
+     */
+    private int shopId;
+    /**
+     * 账户类型：0默认；1团购；2预约预订; 3结婚; 4储值卡
+     */
+    private int businessType;
+    /**
+     * 金额（都是正数）
+     */
+    private BigDecimal flowAmount;
+    /**
+     * 1:正向 2:负向
+     */
+    private int flowType;
+    /**
+     * 源头类型：1.应付2.应收3.交易指令
+     */
+    private SourceType sourceType;
+
+}
