9bbd5b4fa0a1de4d495847a0e2aa6c327b3e6ed9
bingqiu.yuan <bingqiu.yuan@dianping.com>
2013-12-13 18:33:19 +0800

    Signed-off-by: bingqiu.yuan <bingqiu.yuan@dianping.com>

---
+++ ba-finance-exchange-biz/src/test/java/com/dianping/ba/finance/exchange/biz/impl/ShopFundAccountServiceObjectTest.java
@@ -0,0 +1,38 @@
+package com.dianping.ba.finance.exchange.biz.impl;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bingqiu.yuan
+ * Date: 13-12-13
+ * Time: 下午20:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShopFundAccountServiceObjectTest {
+//    String groupID = "1111-1111-1111-1111";
+//    int commonInt = 1;
+//    BigDecimal commonBigDecimal = new BigDecimal(10);
+//    private PCRevenueDAO pcRevenueDaoMock;
+//    private PCActivityDAO pcActivityDAOMock;
+//    private PCRevenueCostServiceObject pcRevenueCostServiceObjectStub;
+
+    @Before
+    public void runBeforeTest() {
+//        pcRevenueDaoMock = mock(PCRevenueDAO.class);
+//        pcActivityDAOMock = mock(PCActivityDAO.class);
+//        pcRevenueCostServiceObjectStub = new PCRevenueCostServiceObject();
+//        pcRevenueCostServiceObjectStub.setPcRevenueDAO(pcRevenueDaoMock);
+//        pcRevenueCostServiceObjectStub.setPcActivityDAO(pcActivityDAOMock);
+    }
+}
