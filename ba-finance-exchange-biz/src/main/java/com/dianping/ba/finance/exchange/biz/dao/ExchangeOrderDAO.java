05ab3dd455fdf9e08dc4e6fcd48abe56a6e55449
bingqiu.yuan <bingqiu.yuan@dianping.com>
2013-12-13 11:40:40 +0800

    dao impl

---
+++ ba-finance-exchange-biz/src/main/java/com/dianping/ba/finance/exchange/biz/dao/ExchangeOrderDAO.java
@@ -0,0 +1,14 @@
+package com.dianping.ba.finance.exchange.biz.dao;
+
+import com.dianping.avatar.dao.GenericDao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bingqiu.yuan
+ * Date: 13-12-13
+ * Time: 上午11:39
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ExchangeOrderDAO extends GenericDao{
+
+}
---
+++ ba-finance-exchange-biz/src/main/java/com/dianping/ba/finance/exchange/biz/impl/ShopFundAccountServiceObject.java
@@ -0,0 +1,14 @@
+package com.dianping.ba.finance.exchange.biz.impl;
+
+import com.dianping.ba.finance.exchange.api.ShopFundAccountService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bingqiu.yuan
+ * Date: 13-12-13
+ * Time: 上午11:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShopFundAccountServiceObject implements ShopFundAccountService {
+
+}
