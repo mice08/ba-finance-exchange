a7b77aef1256fcc156488de4dff33c168b94e333
huajiao.zou <huajiao.zou@dianping.com>
2014-02-11 15:19:35 +0800

    Signed-off-by: huajiao.zou <huajiao.zou@dianping.com>

--- ba-finance-exchange-api/src/main/java/com/dianping/ba/finance/exchange/api/dtos/ExchangeOrderDTO.java
+++ ba-finance-exchange-api/src/main/java/com/dianping/ba/finance/exchange/api/dtos/ExchangeOrderDTO.java
@@ -38,6 +38,25 @@ public class ExchangeOrderDTO implements Serializable {
      */
     private BigDecimal orderAmount;
 
+    private int addLoginId;
+    private int lastUpdateLoginId;
+
+    public int getAddLoginId() {
+        return addLoginId;
+    }
+
+    public void setAddLoginId(int addLoginId) {
+        this.addLoginId = addLoginId;
+    }
+
+    public int getLastUpdateLoginId() {
+        return lastUpdateLoginId;
+    }
+
+    public void setLastUpdateLoginId(int lastUpdateLoginId) {
+        this.lastUpdateLoginId = lastUpdateLoginId;
+    }
+
     public BigDecimal getOrderAmount() {
         return orderAmount;
     }
