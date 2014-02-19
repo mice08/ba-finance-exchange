33d5149728f86e90c7e962cb9ec84e6cffd8db20
bingqiu.yuan <bingqiu.yuan@dianping.com>
2014-02-19 19:08:56 +0800

    sequence
    Signed-off-by: bingqiu.yuan <bingqiu.yuan@dianping.com>

--- ba-finance-exchange-biz/src/main/java/com/dianping/ba/finance/exchange/biz/utils/BizUtils.java
+++ ba-finance-exchange-biz/src/main/java/com/dianping/ba/finance/exchange/biz/utils/BizUtils.java
@@ -1,5 +1,9 @@
 package com.dianping.ba.finance.exchange.biz.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA.
  * User: 遐
@@ -9,7 +13,17 @@ package com.dianping.ba.finance.exchange.biz.utils;
  */
 public class BizUtils {
 
+    /**
+     * 创建sequence
+     * @param clientNo 客户no
+     * @param bizId   id
+     * @return
+     */
     public static String createSequence(String clientNo, String bizId) {
-        return clientNo + "|" + bizId;
+        Date today=Calendar.getInstance().getTime();
+        String todayHour = DateUtils.getFormatDateString(today,"yyyyMMddHH");
+        return clientNo + "|" + bizId+"|"+todayHour;
     }
+
+
 }
