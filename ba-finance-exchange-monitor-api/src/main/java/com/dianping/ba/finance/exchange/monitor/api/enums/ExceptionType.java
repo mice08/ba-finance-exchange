0364a88571004631a718ddad7471f3884538f83b
jingzhou.yan <jingzhou.yan@dianping.com>
2014-03-28 09:39:40 +0800

    add rule and ut Signed-off-by: jingzhou.yan <jingzhou.yan@dianping.com>

--- ba-finance-exchange-monitor-api/src/main/java/com/dianping/ba/finance/exchange/monitor/api/enums/ExceptionType.java
+++ ba-finance-exchange-monitor-api/src/main/java/com/dianping/ba/finance/exchange/monitor/api/enums/ExceptionType.java
@@ -6,7 +6,9 @@ public enum ExceptionType {
     DEFAULT(0),
 
     EO_INITANDPENDING_WITH_NO_FLOW(1),
-    EO_INITANDPENDING_WITH_MORE_FLOW(2);
+    EO_INITANDPENDING_WITH_MORE_FLOW(2),
+    EO_SUCCESS_WITH_NO_MINUSFLOW(3),
+    EO_SUCCESS_WITH_MORE_MINUSFLOW(4);
     private int type;
 
     private ExceptionType(int type) {
@@ -19,6 +21,10 @@ public enum ExceptionType {
                 return EO_INITANDPENDING_WITH_NO_FLOW;
             case 2:
                 return EO_INITANDPENDING_WITH_MORE_FLOW;
+            case 3:
+                return EO_SUCCESS_WITH_NO_MINUSFLOW;
+            case 4:
+                return EO_SUCCESS_WITH_MORE_MINUSFLOW;
             default:
                 return DEFAULT;
         }
@@ -30,6 +36,10 @@ public enum ExceptionType {
                 return "EO状态为初始或者支付中，ShopFundAccountFlow不存在对应的Flow";
             case EO_INITANDPENDING_WITH_MORE_FLOW:
                 return "EO状态为初始或者支付中，ShopFundAccountFlow存在多条对应的Flow";
+            case EO_SUCCESS_WITH_NO_MINUSFLOW:
+                return "EO状态为支付成功，不存在负向流水";
+            case EO_SUCCESS_WITH_MORE_MINUSFLOW:
+                return "EO状态为支付成功，存在多条负向流水";
             default:
                 return "未知错误";
         }
---
+++ ba-finance-exchange-monitor-job/src/main/java/com/dianping/ba/finance/exchange/monitor/job/eocheck/Rule/EOSuccessFlowCheck.java
@@ -0,0 +1,52 @@
+package com.dianping.ba.finance.exchange.monitor.job.eocheck.Rule;
+
+import com.dianping.avatar.log.AvatarLogger;
+import com.dianping.avatar.log.AvatarLoggerFactory;
+import com.dianping.ba.finance.exchange.monitor.api.ShopFundAccountFlowMonitorService;
+import com.dianping.ba.finance.exchange.monitor.api.datas.ExchangeOrderMonitorData;
+import com.dianping.ba.finance.exchange.monitor.api.datas.ShopFundAccountFlowMonitorData;
+import com.dianping.ba.finance.exchange.monitor.api.enums.ExceptionType;
+import com.dianping.ba.finance.exchange.monitor.api.enums.ExchangeOrderStatus;
+import com.dianping.ba.finance.exchange.monitor.job.eocheck.EOCheckBase;
+import com.dianping.ba.finance.exchange.monitor.job.eocheck.EOCheckResult;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jingzhou.yan
+ * Date: 14-3-27
+ * Time: 下午6:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class EOSuccessFlowCheck extends EOCheckBase {
+
+    private static final AvatarLogger MONITOR_LOGGER = AvatarLoggerFactory.getLogger("com.dianping.ba.finance.exchange.monitor.job.eocheck.Rule.EOSuccessFlowCheck");
+    ShopFundAccountFlowMonitorService shopFundAccountFlowMonitorService;
+
+    @Override
+    public boolean filter(ExchangeOrderMonitorData exchangeOrderMonitorData) {
+        return exchangeOrderMonitorData.getStatus() == ExchangeOrderStatus.SUCCESS.value();
+    }
+
+    @Override
+    public EOCheckResult check(ExchangeOrderMonitorData exchangeOrderMonitorData) {
+        // EO状态为支付成功，需要在规定的时间间隔（如5min）存在一条对应的负向Flow。
+        List<ShopFundAccountFlowMonitorData> flowDataList = shopFundAccountFlowMonitorService.findShopFundAccountFlowData(exchangeOrderMonitorData.getEoId());
+        if (CollectionUtils.isEmpty(flowDataList)) {
+            MONITOR_LOGGER.error(String.format("ExchangeOrderMonitorData=%s, exceptionType=%s", exchangeOrderMonitorData, ExceptionType.EO_INITANDPENDING_WITH_NO_FLOW));
+            return createResult(false, checkIfTimeout(exchangeOrderMonitorData.getLastUpdateDate()), ExceptionType.EO_SUCCESS_WITH_NO_MINUSFLOW);
+        } else {
+            if (flowDataList.size() > 1) {
+                MONITOR_LOGGER.error(String.format("ExchangeOrderMonitorData=%s, exceptionType=%s", exchangeOrderMonitorData, ExceptionType.EO_INITANDPENDING_WITH_MORE_FLOW));
+                return createResult(false, checkIfTimeout(exchangeOrderMonitorData.getLastUpdateDate()), ExceptionType.EO_SUCCESS_WITH_MORE_MINUSFLOW);
+            }
+        }
+        return createValidResult();
+    }
+
+    public void setShopFundAccountFlowMonitorService(ShopFundAccountFlowMonitorService shopFundAccountFlowMonitorService) {
+        this.shopFundAccountFlowMonitorService = shopFundAccountFlowMonitorService;
+    }
+}
