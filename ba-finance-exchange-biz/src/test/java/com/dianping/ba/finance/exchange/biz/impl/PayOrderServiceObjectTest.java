745200e4c9dad83b65141ac8513b118aea93deb6
peiqi.shen <peiqi.shen@dianping.com>
2014-06-11 15:38:41 +0800

    add paycode, customerid

--- ba-finance-exchange-biz/src/test/java/com/dianping/ba/finance/exchange/biz/impl/PayOrderServiceObjectTest.java
+++ ba-finance-exchange-biz/src/test/java/com/dianping/ba/finance/exchange/biz/impl/PayOrderServiceObjectTest.java
@@ -7,8 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class PayOrderServiceObjectTest {
@@ -23,7 +22,7 @@ public class PayOrderServiceObjectTest {
     }
 
     @Test
-    public void testCreatePaOrder() {
+    public void testCreatePayOrder() {
         PayOrderData payOrderData = new PayOrderData();
         payOrderData.setAddLoginId(-1);
         payOrderData.setBusinessType(1);
@@ -33,9 +32,18 @@ public class PayOrderServiceObjectTest {
 
         int poId = payOrderServiceObjectStub.createPayOrder(payOrderData);
         Assert.assertEquals(2, poId);
+    }
+
+    @Test
+    public void testCreatePayOrderFailed() {
+        PayOrderData payOrderData = new PayOrderData();
+        payOrderData.setAddLoginId(-1);
+        payOrderData.setBusinessType(1);
+        payOrderData.setPaySequence("1");
 
         when(payOrderDaoMock.insertPayOrder(any(PayOrderData.class))).thenThrow(new RuntimeException("test"));
-        poId = payOrderServiceObjectStub.createPayOrder(payOrderData);
+        int poId = payOrderServiceObjectStub.createPayOrder(payOrderData);
         Assert.assertEquals(-1, poId);
+        verify(payOrderDaoMock, times(5)).insertPayOrder(any(PayOrderData.class));
     }
 }
