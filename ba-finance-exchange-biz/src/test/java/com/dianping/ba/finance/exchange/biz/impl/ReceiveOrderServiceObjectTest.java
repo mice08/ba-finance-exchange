3f151a1fdbe479cbe1ac3e17306f1b3aa6f19adb
peiqi.shen <peiqi.shen@dianping.com>
2014-06-17 18:06:44 +0800

    invoke send message

--- ba-finance-exchange-biz/src/test/java/com/dianping/ba/finance/exchange/biz/impl/ReceiveOrderServiceObjectTest.java
+++ ba-finance-exchange-biz/src/test/java/com/dianping/ba/finance/exchange/biz/impl/ReceiveOrderServiceObjectTest.java
@@ -1,14 +1,16 @@
 package com.dianping.ba.finance.exchange.biz.impl;
 
+import com.dianping.ba.finance.exchange.api.beans.ReceiveOrderResultBean;
 import com.dianping.ba.finance.exchange.api.datas.ReceiveOrderData;
+import com.dianping.ba.finance.exchange.api.enums.ReceiveOrderStatus;
 import com.dianping.ba.finance.exchange.biz.dao.ReceiveOrderDao;
+import com.dianping.ba.finance.exchange.biz.producer.ReceiveOrderResultNotify;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ReceiveOrderServiceObjectTest {
 
@@ -16,12 +18,17 @@ public class ReceiveOrderServiceObjectTest {
 
     private ReceiveOrderDao receiveOrderDaoMock;
 
+    private ReceiveOrderResultNotify receiveOrderResultNotifyMock;
+
     @Before
     public void setUp() throws Exception {
         receiveOrderServiceObjectStub = new ReceiveOrderServiceObject();
 
         receiveOrderDaoMock = mock(ReceiveOrderDao.class);
         receiveOrderServiceObjectStub.setReceiveOrderDao(receiveOrderDaoMock);
+
+        receiveOrderResultNotifyMock = mock(ReceiveOrderResultNotify.class);
+        receiveOrderServiceObjectStub.setReceiveOrderResultNotify(receiveOrderResultNotifyMock);
     }
 
     @Test
@@ -31,9 +38,12 @@ public class ReceiveOrderServiceObjectTest {
         ReceiveOrderData roData = new ReceiveOrderData();
         roData.setCustomerId(123);
         roData.setShopId(123);
+        roData.setStatus(ReceiveOrderStatus.CONFIRMED.value());
         int roId = receiveOrderServiceObjectStub.createReceiveOrder(roData);
         Assert.assertEquals(87, roId);
         Assert.assertEquals(87, roData.getRoId());
+
+        verify(receiveOrderResultNotifyMock, times(1)).receiveResultNotify(any(ReceiveOrderResultBean.class));
     }
 
 }
