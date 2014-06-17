1216aa3dd948fc306928dd3da59503b82d843a42
peiqi.shen <peiqi.shen@dianping.com>
2014-06-17 18:34:50 +0800

    add receive type

--- ba-finance-exchange-api/src/main/java/com/dianping/ba/finance/exchange/api/enums/ReceiveType.java
+++ ba-finance-exchange-api/src/main/java/com/dianping/ba/finance/exchange/api/enums/ReceiveType.java
@@ -9,10 +9,36 @@ public enum ReceiveType {
 	 * 0-默认错误
 	 */
 	DEFAULT(0, "默认错误"),
+
 	/**
-     * 1-推广-广告款
+     * 1-广告-广告款
      */
-    AD_FEE(1, "推广-广告款");
+    AD_FEE(1, "广告-广告款"),
+
+	/**
+     * 2-团购-上架费
+     */
+    TG_SHELVING_FEE(2, "团购-上架费"),
+
+	/**
+     * 3-团购-商户押金
+     */
+    TG_DEPOSIT(3, "团购-商户押金"),
+
+	/**
+     * 4-团购-保证金
+     */
+    TG_SECURITY_DEPOSIT(4, "团购-保证金"),
+
+	/**
+     * 5-团购-保底款
+     */
+    TG_GUARANTEE(5, "团购-保底款"),
+
+	/**
+     * 6-团购-多付款
+     */
+    TG_EXTRA_FEE(6, "团购-多付款");
 
     private int receiveType;
 
