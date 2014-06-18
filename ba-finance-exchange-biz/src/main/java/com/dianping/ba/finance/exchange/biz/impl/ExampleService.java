10305891aebb17284362aeb345af5e91b6872bcf
peiqi.shen <peiqi.shen@dianping.com>
2014-06-18 10:07:28 +0800

    add groovy ut

--- ba-finance-exchange-biz/src/main/java/com/dianping/ba/finance/exchange/biz/impl/ExampleService.java
+++ ba-finance-exchange-biz/src/main/java/com/dianping/ba/finance/exchange/biz/impl/ExampleService.java
@@ -1,9 +1,16 @@
 package com.dianping.ba.finance.exchange.biz.impl;
 
 
+import com.dianping.ba.finance.exchange.api.datas.ExchangeOrderData;
+import com.dianping.ba.finance.exchange.biz.dao.ExchangeOrderDao;
+
+import java.util.List;
+
 public class ExampleService {
 
     private String serviceName;
+
+    private ExchangeOrderDao exchangeOrderDao;
 
     public String hello(String from) {
         return String.format("From=%s, ServiceName=%s", from, serviceName);
@@ -11,5 +18,14 @@ public class ExampleService {
 
     public void setServiceName(String serviceName) {
         this.serviceName = serviceName;
+    }
+
+    public List<ExchangeOrderData> findExchangeOrderListByOrderIdList(List<Integer> orderIdList) {
+        return exchangeOrderDao.findExchangeOrderListByOrderIdList(orderIdList);
+    }
+
+
+    public void setExchangeOrderDao(ExchangeOrderDao exchangeOrderDao) {
+        this.exchangeOrderDao = exchangeOrderDao;
     }
 }
--- ba-finance-exchange-biz/src/test/groovy/com/dianping/ba/finance/exchange/biz/impl/ExampleServiceTest.groovy
+++ ba-finance-exchange-biz/src/test/groovy/com/dianping/ba/finance/exchange/biz/impl/ExampleServiceTest.groovy
@@ -1,18 +1,59 @@
 package com.dianping.ba.finance.exchange.biz.impl
+import com.dianping.ba.finance.exchange.api.datas.ExchangeOrderData
+import com.dianping.ba.finance.exchange.biz.dao.ExchangeOrderDao
+import groovy.mock.interceptor.MockFor
+import org.junit.Before
+import org.junit.Test
+
+import static org.mockito.Matchers.anyList
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.when
 
 /**
  * Created by noahshen on 14-5-26.
  */
-class ExampleServiceTest extends GroovyTestCase {
+class ExampleServiceTest {
 
     def exampleServiceStub = new ExampleService();
+
+    private ExchangeOrderDao exchangeOrderDaoMock;
+
+    @Before
     void setUp() {
-        super.setUp()
         exampleServiceStub.setServiceName("exampleService");
+        exchangeOrderDaoMock = mock(ExchangeOrderDao.class);
+        exampleServiceStub.setExchangeOrderDao(exchangeOrderDaoMock);
     }
 
+    @Test
     void testHello() {
         def result = exampleServiceStub.hello("noah");
         assert result == "From=noah, ServiceName=exampleService"
+    }
+
+    @Test
+    void testFindExchangeOrderListByOrderIdList() {
+        ExchangeOrderData eoData = [exchangeOrderId: 123, bankName: "bankName" ] as ExchangeOrderData;
+        when(exchangeOrderDaoMock.findExchangeOrderListByOrderIdList(anyList())).thenReturn(Arrays.asList(eoData));
+
+        List<ExchangeOrderData> eoDataList = exampleServiceStub.findExchangeOrderListByOrderIdList(Arrays.asList(123));
+        assert !eoDataList.isEmpty()
+        assert eoDataList.get(0).getExchangeOrderId() == 123
+    }
+
+    @Test
+    void testFindExchangeOrderListByOrderIdListWithGMock() {
+        ExchangeOrderData eoData = [exchangeOrderId: 8787, bankName: "bankName" ] as ExchangeOrderData;
+
+        MockFor exchangeOrderDaoMock = new MockFor(ExchangeOrderDao)
+        exchangeOrderDaoMock.demand.findExchangeOrderListByOrderIdList {
+            Arrays.asList(eoData)
+        }
+        exampleServiceStub.setExchangeOrderDao(exchangeOrderDaoMock.proxyDelegateInstance());
+
+        List<ExchangeOrderData> eoDataList = exampleServiceStub.findExchangeOrderListByOrderIdList(Arrays.asList(123));
+        assert !eoDataList.isEmpty()
+        assert eoDataList.get(0).getExchangeOrderId() == 8787
+
     }
 }
