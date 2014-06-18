7f12f5df4137497e0f8fa994d774747f7a008755
qihong.yu <qihong.yu@dianping.com>
2014-06-18 18:19:38 +0800

    modify bigdecimal format
    Signed-off-by: qihong.yu <qihong.yu@dianping.com>

--- ba-finance-exchange-web/src/test/java/com/dianping/ba/finance/exchange/siteweb/action/ajax/ReceiveOrderAjaxActionTest.java
+++ ba-finance-exchange-web/src/test/java/com/dianping/ba/finance/exchange/siteweb/action/ajax/ReceiveOrderAjaxActionTest.java
@@ -1,21 +1,22 @@
 package com.dianping.ba.finance.exchange.siteweb.action.ajax;
 
 import com.dianping.ba.finance.exchange.api.ReceiveOrderService;
+import com.dianping.ba.finance.exchange.api.beans.ReceiveOrderSearchBean;
 import com.dianping.ba.finance.exchange.api.datas.ReceiveOrderData;
 import com.dianping.ba.finance.exchange.api.enums.BusinessType;
 import com.dianping.ba.finance.exchange.api.enums.ReceiveOrderPayChannel;
 import com.dianping.ba.finance.exchange.api.enums.ReceiveType;
+import com.dianping.core.type.PageModel;
 import com.opensymphony.xwork2.Action;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class ReceiveOrderAjaxActionTest {
 
@@ -29,6 +30,23 @@ public class ReceiveOrderAjaxActionTest {
 
         receiveOrderServiceMock = mock(ReceiveOrderService.class);
         receiveOrderAjaxActionStub.setReceiveOrderService(receiveOrderServiceMock);
+    }
+
+    @Test
+    public void testJsonExecute() throws Exception {
+        PageModel pageModel = new PageModel();
+        ReceiveOrderData receiveOrderData=new ReceiveOrderData();
+        pageModel.setRecords(Arrays.asList(receiveOrderData));
+        when(receiveOrderServiceMock.paginateReceiveOrderList(new ReceiveOrderSearchBean(),1,1)).thenReturn(pageModel);
+        when(receiveOrderServiceMock.loadReceiveOrderTotalAmountByCondition(new ReceiveOrderSearchBean())).thenReturn(BigDecimal.ONE);
+
+        receiveOrderAjaxActionStub.setBusinessType(0);
+        receiveOrderAjaxActionStub.jsonExecute();
+
+        receiveOrderAjaxActionStub.setBusinessType(1);
+        receiveOrderAjaxActionStub.jsonExecute();
+
+        Assert.assertEquals(receiveOrderAjaxActionStub.getTotalAmount(),"0.00");
     }
 
     @Test
