1123cef1f6bae6309d3d1918e9cd8ae77ddd2abc
peiqi.shen <peiqi.shen@dianping.com>
2014-06-18 10:49:36 +0800

    add searchbean

---
+++ ba-finance-exchange-api/src/main/java/com/dianping/ba/finance/exchange/api/beans/ReceiveOrderSearchBean.java
@@ -0,0 +1,56 @@
+package com.dianping.ba.finance.exchange.api.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Eric on 2014/6/11.
+ */
+public class ReceiveOrderSearchBean implements Serializable {
+
+    private int businessType;
+    private Date beginTime;
+    private Date endTime;
+    private int status;
+    private String payCode;
+
+    public int getBusinessType() {
+        return businessType;
+    }
+
+    public void setBusinessType(int businessType) {
+        this.businessType = businessType;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getPayCode() {
+        return payCode;
+    }
+
+    public void setPayCode(String payCode) {
+        this.payCode = payCode;
+    }
+
+    public Date getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(Date beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
