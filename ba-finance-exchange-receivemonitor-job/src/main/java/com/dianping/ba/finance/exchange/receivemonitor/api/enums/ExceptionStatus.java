c8a3b3c50819e70631a90127421c11e47af389bf
yaxiong.cheng <yaxiong.cheng@dianping.com>
2014-08-11 18:32:18 +0800

    assembly.xml added modified enum classes, field "description" added

--- ba-finance-exchange-receivemonitor-job/src/main/java/com/dianping/ba/finance/exchange/receivemonitor/api/enums/ExceptionStatus.java
+++ ba-finance-exchange-receivemonitor-job/src/main/java/com/dianping/ba/finance/exchange/receivemonitor/api/enums/ExceptionStatus.java
@@ -2,14 +2,23 @@ package com.dianping.ba.finance.exchange.receivemonitor.api.enums;
 
 public enum ExceptionStatus {
 
-    INIT(1),
+	/**
+	 * 1-初始化
+	 */
+    INIT(1,"初始化"),
 
-    HANDLED(2);
+	/**
+	 * 2-已处理
+	 */
+    HANDLED(2,"已处理");
 
     private int status;
 
-    private ExceptionStatus(int status) {
+	private String description;
+
+    private ExceptionStatus(int status,String desc) {
         this.status = status;
+		this.description = desc;
     }
 
     public int getStatus() {
@@ -23,4 +32,12 @@ public enum ExceptionStatus {
     public int value(){
         return this.status;
     }
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- ba-finance-exchange-receivemonitor-job/src/main/java/com/dianping/ba/finance/exchange/receivemonitor/api/enums/TodoStatus.java
+++ ba-finance-exchange-receivemonitor-job/src/main/java/com/dianping/ba/finance/exchange/receivemonitor/api/enums/TodoStatus.java
@@ -3,14 +3,23 @@ package com.dianping.ba.finance.exchange.receivemonitor.api.enums;
 
 public enum TodoStatus {
 
-    INIT(1),
+	/**
+	 * 1-初始状态
+	 */
+    INIT(1,"初始"),
 
-    HANDLED(2);
+	/**
+	 * 2—已处理
+	 */
+    HANDLED(2,"已处理");
 
     private int status;
 
-    private TodoStatus(int status) {
+	private String description;
+
+    private TodoStatus(int status,String description) {
         this.status = status;
+		this.description = description;
     }
 
     public int getStatus() {
@@ -24,4 +33,12 @@ public enum TodoStatus {
     public int value(){
         return this.status;
     }
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
