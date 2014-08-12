f6f34ce604b1975735e17cf859783cd50319dfc1
peiqi.shen <peiqi.shen@dianping.com>
2014-08-12 10:58:01 +0800

    optimize submitting

--- ba-finance-exchange-web/src/test/java/com/dianping/ba/finance/exchange/siteweb/action/ajax/PaySuccessAjaxActionTest.java
+++ ba-finance-exchange-web/src/test/java/com/dianping/ba/finance/exchange/siteweb/action/ajax/PaySuccessAjaxActionTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -58,23 +59,19 @@ public class PaySuccessAjaxActionTest {
 		String actual = paySuccessAjaxActionStub.submitPaySuccess();
 
 		assertEquals("success", actual);
-		verify(payOrderServiceMock, atLeastOnce()).findPayOrderList(any(PayOrderSearchBean.class));
+		verify(payOrderServiceMock, atLeastOnce()).findPayOrderIdList(any(PayOrderSearchBean.class));
 		verify(payOrderServiceMock, never()).updatePayOrderToPaySuccess(anyList(), anyInt());
 	}
 
 	@Test
 	public void testSubmitPaySuccessByConditionStatusIsValid() {
-		List<PayOrderData> dataList = new ArrayList<PayOrderData>();
-		PayOrderData data = new PayOrderData();
-		data.setStatus(PayOrderStatus.EXPORT_PAYING.value());
-		dataList.add(data);
-		when(payOrderServiceMock.findPayOrderList(any(PayOrderSearchBean.class))).thenReturn(dataList);
+		when(payOrderServiceMock.findPayOrderIdList(any(PayOrderSearchBean.class))).thenReturn(Arrays.asList(123));
 		when(payOrderServiceMock.updatePayOrderToPaySuccess(anyList(), anyInt())).thenReturn(1);
 
 		String actual = paySuccessAjaxActionStub.submitPaySuccess();
 
 		assertEquals("success", actual);
-		verify(payOrderServiceMock, atLeastOnce()).findPayOrderList(any(PayOrderSearchBean.class));
+		verify(payOrderServiceMock, atLeastOnce()).findPayOrderIdList(any(PayOrderSearchBean.class));
 		verify(payOrderServiceMock, atLeastOnce()).updatePayOrderToPaySuccess(anyList(), anyInt());
 	}
 
@@ -90,7 +87,7 @@ public class PaySuccessAjaxActionTest {
 		String actual = paySuccessAjaxActionStub.submitPaySuccess();
 
 		assertEquals("success", actual);
-		verify(payOrderServiceMock, atLeastOnce()).findPayOrderList(any(PayOrderSearchBean.class));
+		verify(payOrderServiceMock, atLeastOnce()).findPayOrderIdList(any(PayOrderSearchBean.class));
 		verify(payOrderServiceMock, never()).updatePayOrderToPaySuccess(anyList(), anyInt());
 	}
 }
