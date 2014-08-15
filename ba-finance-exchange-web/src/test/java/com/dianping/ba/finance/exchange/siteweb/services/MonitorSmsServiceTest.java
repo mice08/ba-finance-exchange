92e75908760abc3a750597199362b9946c14d27b
adam.huang <adam.huang@dianping.com>
2014-08-15 18:59:21 +0800

    add ut

---
+++ ba-finance-exchange-web/src/test/java/com/dianping/ba/finance/exchange/siteweb/services/MonitorSmsServiceTest.java
@@ -0,0 +1,34 @@
+package com.dianping.ba.finance.exchange.siteweb.services;
+
+import com.dianping.sms.biz.SMSService;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.HashMap;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MonitorSmsServiceTest {
+
+    private MonitorSmsService monitorSmsServiceStub;
+    private SMSService smsServiceMock;
+
+    @Before
+    public void runBeforeTest() {
+        smsServiceMock = mock(SMSService.class);
+        monitorSmsServiceStub = new MonitorSmsService();
+        monitorSmsServiceStub.setSmsService(smsServiceMock);
+    }
+
+    @Test
+    public void sendSms() throws ParseException {
+        HashMap<String, String> contentMap = new HashMap<String, String>();
+        contentMap.put("content", "");
+        when(smsServiceMock.send(813, "", contentMap)).thenReturn(1);
+        boolean actual = monitorSmsServiceStub.sendSms("");
+        Assert.assertTrue(actual);
+    }
+}
