eef556e6c98da69169e4ad6d0b527f2ae1cf8a7c
bingqiu.yuan <bingqiu.yuan@dianping.com>
2014-08-21 19:35:14 +0800

    job

--- ba-finance-exchange-voucher-job/src/main/java/com/dianping/ba/finance/exchange/voucher/job/NoContractReceiveVoucherController.java
+++ ba-finance-exchange-voucher-job/src/main/java/com/dianping/ba/finance/exchange/voucher/job/NoContractReceiveVoucherController.java
@@ -3,6 +3,9 @@ package com.dianping.ba.finance.exchange.voucher.job;
 import com.dianping.avatar.log.AvatarLogger;
 import com.dianping.avatar.log.AvatarLoggerFactory;
 import com.dianping.ba.finance.exchange.api.ReceiveVoucherService;
+import com.dianping.finance.common.util.DateUtils;
+import com.dianping.finance.common.util.LionConfigUtils;
+import com.site.lookup.util.StringUtils;
 
 import java.util.Date;
 
@@ -14,7 +17,14 @@ public class NoContractReceiveVoucherController {
 	public boolean execute() {
         MONITOR_LOGGER.info(String.format("NoContractReceiveVoucherController.execute start"));
         try{
-            receiveVoucherService.generateUnconfirmedReceiveVoucher(new Date());
+            //当前日期减一天
+            String strRunDate=DateUtils.format(DateUtils.addDate(DateUtils.getCurrentDate(),-1));
+            String lionDate=LionConfigUtils.getProperty("ba-finance-exchange-voucher-job.runDate","");
+            if (StringUtils.isNotEmpty(lionDate)) {
+                strRunDate=lionDate;
+            }
+            Date runDate= DateUtils.formatDate(strRunDate);
+            receiveVoucherService.generateUnconfirmedReceiveVoucher(runDate);
         }catch (Exception e){
             MONITOR_LOGGER.error(String.format("severity=[2] NoContractReceiveVoucherController.execute error"), e);
         }
