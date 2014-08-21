4d4de5bbb4a78e906a4b71de8cb360802387d2de
bingqiu.yuan <bingqiu.yuan@dianping.com>
2014-08-21 18:36:13 +0800

    interface
    Signed-off-by: bingqiu.yuan <bingqiu.yuan@dianping.com>

--- ba-finance-exchange-voucher-job/src/main/java/com/dianping/ba/finance/exchange/voucher/job/NoContractReceiveVoucherController.java
+++ ba-finance-exchange-voucher-job/src/main/java/com/dianping/ba/finance/exchange/voucher/job/NoContractReceiveVoucherController.java
@@ -2,20 +2,26 @@ package com.dianping.ba.finance.exchange.voucher.job;
 
 import com.dianping.avatar.log.AvatarLogger;
 import com.dianping.avatar.log.AvatarLoggerFactory;
+import com.dianping.ba.finance.exchange.api.ReceiveVoucherService;
 
-import java.util.Calendar;
 import java.util.Date;
 
 
 public class NoContractReceiveVoucherController {
-    private static final AvatarLogger monitorLogger = AvatarLoggerFactory.getLogger("com.dianping.ba.finance.exchange.voucher.job.NoContractReceiveVoucherController");
+    private static final AvatarLogger MONITOR_LOGGER = AvatarLoggerFactory.getLogger("com.dianping.ba.finance.exchange.voucher.job.NoContractReceiveVoucherController");
+    private ReceiveVoucherService receiveVoucherService;
 
 	public boolean execute() {
-		monitorLogger.info("Start to monitor....");
-		Date currentMonitorTime = Calendar.getInstance().getTime();
-		long startTime = System.currentTimeMillis();
-
+        MONITOR_LOGGER.info(String.format("NoContractReceiveVoucherController.execute start"));
+        try{
+            receiveVoucherService.generateUnconfirmedReceiveVoucher(new Date());
+        }catch (Exception e){
+            MONITOR_LOGGER.error(String.format("severity=[2] NoContractReceiveVoucherController.execute error"), e);
+        }
         return true;
 	}
 
+    public void setReceiveVoucherService(ReceiveVoucherService receiveVoucherService) {
+        this.receiveVoucherService = receiveVoucherService;
+    }
 }
