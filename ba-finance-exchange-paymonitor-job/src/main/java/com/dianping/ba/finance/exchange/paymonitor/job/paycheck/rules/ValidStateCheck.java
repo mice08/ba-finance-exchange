2f623e070e4011234006cb17b329456f01d3b062
adam.huang <adam.huang@dianping.com>
2014-09-15 12:32:18 +0800

    fix

--- ba-finance-exchange-paymonitor-job/src/main/java/com/dianping/ba/finance/exchange/paymonitor/job/paycheck/rules/ValidStateCheck.java
+++ ba-finance-exchange-paymonitor-job/src/main/java/com/dianping/ba/finance/exchange/paymonitor/job/paycheck/rules/ValidStateCheck.java
@@ -1,5 +1,7 @@
 package com.dianping.ba.finance.exchange.paymonitor.job.paycheck.rules;
 
+import com.dianping.avatar.log.AvatarLogger;
+import com.dianping.avatar.log.AvatarLoggerFactory;
 import com.dianping.ba.finance.exchange.paymonitor.api.datas.PayPlanMonitorData;
 import com.dianping.ba.finance.exchange.paymonitor.api.enums.PayPlanStatus;
 import com.dianping.ba.finance.exchange.paymonitor.job.paycheck.BasePayCheckRule;
@@ -9,6 +11,8 @@ import com.dianping.ba.finance.exchange.paymonitor.job.paycheck.PayCheckResult;
  * Created by adam.huang on 2014/8/8.
  */
 public class ValidStateCheck extends BasePayCheckRule {
+    private static final AvatarLogger MONITOR_LOGGER = AvatarLoggerFactory.getLogger("com.dianping.ba.finance.exchange.paymonitor.job.PayPlanDataChecker");
+
     @Override
     public boolean filter(PayPlanMonitorData payPlanMonitorData) {
         if(payPlanMonitorData.getStatus() == PayPlanStatus.INIT.value() ||
@@ -21,6 +25,7 @@ public class ValidStateCheck extends BasePayCheckRule {
 
     @Override
     public PayCheckResult check(PayPlanMonitorData payPlanMonitorData) {
+        MONITOR_LOGGER.info("============ValidStateCheck===============");
         return createValidResult();
     }
 }
