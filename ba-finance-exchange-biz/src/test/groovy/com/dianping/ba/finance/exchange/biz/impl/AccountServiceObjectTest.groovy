28faad16e44f73a92a4b1c1aec4a0cd9f118567f
JerryBlight <qihoong891009@gmail.com>
2015-03-09 18:17:42 +0800

    account

--- ba-finance-exchange-biz/src/test/groovy/com/dianping/ba/finance/exchange/biz/impl/AccountServiceObjectTest.groovy
+++ ba-finance-exchange-biz/src/test/groovy/com/dianping/ba/finance/exchange/biz/impl/AccountServiceObjectTest.groovy
@@ -1,5 +1,10 @@
 package com.dianping.ba.finance.exchange.biz.impl
 
+import com.dianping.ba.finance.exchange.api.beans.AccountUpdateInfoBean
+import com.dianping.ba.finance.exchange.api.datas.AccountData
+import com.dianping.ba.finance.exchange.api.datas.AccountEntryData
+import com.dianping.ba.finance.exchange.api.datas.BankAccountData
+import com.dianping.ba.finance.exchange.api.dtos.AccountEntryRequestDTO
 import com.dianping.ba.finance.exchange.biz.dao.AccountDao
 import com.dianping.ba.finance.exchange.biz.dao.AccountEntryDao
 import com.dianping.ba.finance.exchange.biz.dao.BankAccountDao
@@ -26,14 +31,50 @@ class AccountServiceObjectTest extends Specification {
     }
 
     def "UpdateAccount"() {
-
+        setup:
+        accountDaoMock.loadAccountByBankAccount(_ as Integer) >> {
+            [direction:1, balance:1]
+        }
+        when:
+        accountServiceObjectStub.updateAccount([sourceType:1,amount:1] as AccountEntryRequestDTO)
+        then:
+        1* accountDaoMock.updateAccount(_ as AccountUpdateInfoBean)
     }
 
-    def "FindAccountEntryByTime"() {
+    def "FindAccountEntryByTime"(Integer size, Integer actual) {
+        given:
+        accountEntryDaoMock.findAccountEntryByTime(_ as Integer, 1, _ as Date, _ as Date) >> {
+            AccountEntryData data = [id: 1]
+            [data]
+        }
+        accountEntryDaoMock.findAccountEntryByTime(_ as Integer, 0, _ as Date, _ as Date) >> {
+            []
+        }
+        expect:
+        actual == accountServiceObjectStub.findAccountEntryByTime(1, size, new Date(), new Date()).size()
 
+        where:
+        size | actual
+        1    | 1
+        0    | 0
     }
 
-    def "LoadBankAccount"() {
-
+    def "LoadBankAccount"(Integer id, Boolean result) {
+        given:
+        bankAccountDaoMock.loadBankAccount(1) >> {
+            BankAccountData data = [id: 1]
+            data
+        }
+        accountDaoMock.loadAccountByBankAccount(0) >> {
+            null
+        }
+        expect:
+        result == (accountServiceObjectStub.loadBankAccount(id) != null)
+        where:
+        id | result
+        1  | true
+        0  | false
     }
+
+
 }
--- ba-finance-exchange-mq/src/main/java/com/dianping/ba/finance/exchange/mq/listener/BankPayResultListener.java
+++ ba-finance-exchange-mq/src/main/java/com/dianping/ba/finance/exchange/mq/listener/BankPayResultListener.java
@@ -2,16 +2,12 @@ package com.dianping.ba.finance.exchange.mq.listener;
 
 import com.dianping.avatar.log.AvatarLogger;
 import com.dianping.avatar.log.AvatarLoggerFactory;
-import com.dianping.ba.finance.exchange.api.BankPayResultHandleService;
-import com.dianping.ba.finance.exchange.api.PayOrderService;
+import com.dianping.ba.finance.exchange.api.PayOrderDomainService;
 import com.dianping.ba.finance.exchange.api.dtos.BankPayResultDTO;
-import com.dianping.ba.finance.exchange.api.enums.PayOrderStatus;
 import com.dianping.finance.common.aop.annotation.Log;
 import com.dianping.finance.common.swallow.SwallowMessageListener;
 import com.dianping.swallow.common.message.Message;
 import com.dianping.swallow.consumer.BackoutMessageException;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -22,14 +18,14 @@ public class BankPayResultListener extends SwallowMessageListener {
     private static final AvatarLogger MONITOR_LOGGER = AvatarLoggerFactory.getLogger("com.dianping.ba.finance.exchange.mq.monitor.PayRequestResultListener");
 
     @Autowired
-    private BankPayResultHandleService bankPayResultHandleService;
+    private PayOrderDomainService payOrderDomainService;
 
     @Log(logBefore = true, logAfter = true, severity = 1)
     @Override
     public void onMessage(Message message) throws BackoutMessageException {
         MONITOR_LOGGER.info(String.format("BankPayResultListener.onMessage, message=%s", message));
         try {
-            bankPayResultHandleService.handleBankPayResult(message.transferContentToBean(BankPayResultDTO.class));
+            payOrderDomainService.handleBankPayResult(message.transferContentToBean(BankPayResultDTO.class));
         } catch (Exception e) {
             MONITOR_LOGGER.error(String.format("severity=[2] BankPayResultListener.onMessage error, message=%s", message), e);
             throw new BackoutMessageException(e);
