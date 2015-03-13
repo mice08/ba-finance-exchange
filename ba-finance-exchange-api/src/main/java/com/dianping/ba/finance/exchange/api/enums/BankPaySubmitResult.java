d94e45e10433dba8c62b85bf8e8d17e5b2db534f
JerryBlight <qihoong891009@gmail.com>
2015-03-13 19:22:51 +0800

    add pom name

---
+++ ba-finance-exchange-api/src/main/java/com/dianping/ba/finance/exchange/api/enums/BankPaySubmitResult.java
@@ -0,0 +1,7 @@
+package com.dianping.ba.finance.exchange.api.enums;
+
+/**
+ * Created by will on 15-3-13.
+ */
+public enum BankPaySubmitResult {
+}
---
+++ ba-finance-exchange-web/src/main/java/com/dianping/ba/finance/exchange/siteweb/action/ajax/SendSMSAuthCodeAjaxAction.java
@@ -0,0 +1,7 @@
+package com.dianping.ba.finance.exchange.siteweb.action.ajax;
+
+/**
+ * Created by will on 15-3-13.
+ */
+public class SendSMSAuthCodeAjaxAction {
+}
